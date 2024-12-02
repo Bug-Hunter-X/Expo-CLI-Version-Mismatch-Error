69d03e3d3186543ca83bddf1e0102501fcddfac1
BugHunter-X <askzenai@gmail.com>
2024-12-03 00:42:40 +0100

    Add expoBug.js file

---
+++ expoBug.js
@@ -0,0 +1,3 @@
+This error typically occurs when there's a mismatch between the Expo CLI version and the dependencies specified in your project's `package.json` file.  It often manifests when you try to run `expo start` or other Expo CLI commands.
+
+For example, your project might require a newer version of a dependency than what your current Expo CLI supports. Or, conversely, you might be using an older Expo CLI with a project that requires features only available in a newer version.
